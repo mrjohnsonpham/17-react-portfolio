81e08259b4201126a11840ca5697e84b447aa263
Johnson Pham <mrjohnsonpham@gmail.com>
2020-10-26 22:30:35 -0400

    Fixed search results

--- src/pages/All/ResultsArray.tsx
+++ src/pages/All/ResultsArray.tsx
@@ -7,48 +7,43 @@ const ResultsAray = [
   {
     title: 'Software Engineer Intern, ',
     link: 'https://www.ibm.com/cloud',
-    description: `Developed and maintained various microservices using Elixir, Python, Ruby, and Node. Deployed microservices to Kubernetes with Helm, built various frontend components with ReactJS and VueJS...`,
+    description: `Lorem ipsum...`,
   },
   {
-    title: 'Lead Developer, Network Re-Engineered Inc',
+    title: 'Lead Developer',
     link: 'https://www.campusconnect.ca/company',
-    description: `Working on the rewrite of our Campus Connect application using React Native, Typescript, Firebase and Redux...`,
+    description: `Lorem Ipsum...`,
   },
   {
-    title: 'Project Lead for all 3 projects',
+    title: 'Project Lead: 3 projects',
     link: 'https://bootcamp.rutgers.edu/',
     description: `Lead a team of four in the development of each Application to participate in Rutgers 2020 Full Stack Development Course...`,
   },
   {
-    title: 'Front End Developer, Illustrious Comics',
-    link: 'https://www.utsc.utoronto.ca/thehub/startup-overview',
-    description: `Developed the Front End for a comic reader web application using NextJS and TypeScript, with an AWS graphQL backend. Developed frontend payment integration with StripeJS...`,
+    title: 'Front End Developer',
+    link: 'https://www.uniqlo.com',
+    description: `Developed the Front End ...`,
   },
   {
     title:
-      'IT Director, Association of Mathematical and Computer Science Students',
-    link: 'https://www.amacss.org/',
-    description: `Oversaw the Front End team in development of the organization's new website, currently building a database management system to store club membership information...`,
+      'Something',
+    link: 'https://google.com',
+    description: `Oversaw the Front End team in development of the ...`,
   },
   {
-    title: 'Fry Cook, Highland Fish and Chips',
-    link: 'https://www.yelp.ca/biz/highland-fish-and-chips-scarborough/',
-    description: `Prepped and Portioned Halibut, Haddock and Cod for service, in charge of restaurant oil/lard management...`,
+    title: 'HeadCook, Unphogettable',
+    link: 'https://www.yelp.com',
+    description: `Unforgettable Vietnamese Pho and rice dishes...`,
   },
   {
-    title: 'Line Cook, Via Cibo | Italian Street Food',
-    link: 'https://www.viacibo.com/',
-    description: `Prepared and maintained ingredient levels on the line, prepared menu items in co-operation with others. Able to rotate on all cooking stations: Pizza, Sandwich/Salads, Saute/Pasta and Brunch.
-                    Main cooking repertoire includes a large variety of traditional italian dishes ranging from Neapolitan Pizzas, Risottos to Bomboloni...`,
+    title: 'Phamous Brand',
+    link: 'https://www.google.com',
+    description: `Clothing brand that reminds you of all the accomplishments you've done. You're familiar to someone...`,
   },
   {
-    title: 'Sous Chef, The Roux',
-    link: 'http://www.rouxstir.ca/',
-    description: `Worked alongside chef de cuisine to manage kitchen 
-                    activities including staff, menu planning, 
-                    sanitary/safety standards as well as overall restaurant performance.
-                    In charge of all restaurant pastries including: Pizza Dough, 
-                    Croissants, 3 types of Waffle Mix, Beignets, Scones... Main cooking repertoire includes a large variety of traditional French and southern inspired dishes.`,
+    title: 'Corgi Expert',
+    link: 'http://www.corgi.com',
+    description: `Dog Show Contest Specialist.`,
   },
 ];
 

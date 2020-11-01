bb1497d57bd9641b8547bafd55bc0c1a74c91e39
Johnson Pham <mrjohnsonpham@gmail.com>
2020-11-01 15:42:29 -0500

    Deployment

---
+++ docs/precache-manifest.fb3ab538ad38774d1ab2e0abf0e475a6.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c98854b97f1526a3092038f481f608",
+    "url": "/17-react-portfolio/index.html"
+  },
+  {
+    "revision": "ec5453ce4636891d1319",
+    "url": "/17-react-portfolio/static/css/main.82fc8cd6.chunk.css"
+  },
+  {
+    "revision": "19b9a8fe3794690d86ac",
+    "url": "/17-react-portfolio/static/js/2.033b3e5b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/17-react-portfolio/static/js/2.033b3e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5453ce4636891d1319",
+    "url": "/17-react-portfolio/static/js/main.6d742fba.chunk.js"
+  },
+  {
+    "revision": "c8d2d962ecc3e1c5c0c0",
+    "url": "/17-react-portfolio/static/js/runtime-main.eeb95048.js"
+  },
+  {
+    "revision": "9d95e4b10b030782648a193946d35389",
+    "url": "/17-react-portfolio/static/media/1.9d95e4b1.jpg"
+  },
+  {
+    "revision": "9216df36004ee50a3259fdc71fd26f1d",
+    "url": "/17-react-portfolio/static/media/2.9216df36.jpg"
+  },
+  {
+    "revision": "465a47025c459433d6540e6ebe1b811e",
+    "url": "/17-react-portfolio/static/media/3.465a4702.jpg"
+  },
+  {
+    "revision": "0b5478dc236a782f939b086a07981f84",
+    "url": "/17-react-portfolio/static/media/4.0b5478dc.jpg"
+  },
+  {
+    "revision": "65ed86759d37092ab886b7e398c3781d",
+    "url": "/17-react-portfolio/static/media/5.65ed8675.jpg"
+  },
+  {
+    "revision": "e32f4ade637c1f1d5cbf1d8bb78c9d93",
+    "url": "/17-react-portfolio/static/media/6.e32f4ade.jpg"
+  },
+  {
+    "revision": "b6f6ee940a052afaa9208154e1028ff6",
+    "url": "/17-react-portfolio/static/media/book-active.b6f6ee94.svg"
+  },
+  {
+    "revision": "39725701e48e5dfb285b3e83970ec529",
+    "url": "/17-react-portfolio/static/media/book.39725701.svg"
+  },
+  {
+    "revision": "60e26b4d17188eb8725eec962e0a4e11",
+    "url": "/17-react-portfolio/static/media/bryan.60e26b4d.jpg"
+  },
+  {
+    "revision": "cfa432763442c0c4d390376e74fc13b1",
+    "url": "/17-react-portfolio/static/media/dots.cfa43276.svg"
+  },
+  {
+    "revision": "7dcfb56a52cda8a6684ea79e88fa32f2",
+    "url": "/17-react-portfolio/static/media/finance-active.7dcfb56a.svg"
+  },
+  {
+    "revision": "eddf5590579f416f904b92dcba2d9433",
+    "url": "/17-react-portfolio/static/media/finance.eddf5590.svg"
+  },
+  {
+    "revision": "562368f92883b720b427ae6d4a2b22b0",
+    "url": "/17-react-portfolio/static/media/icon.562368f9.svg"
+  },
+  {
+    "revision": "64ea64d4f2b2280ae14546ff3400868d",
+    "url": "/17-react-portfolio/static/media/image-active.64ea64d4.svg"
+  },
+  {
+    "revision": "af826f61609dde6459f7c23592270d87",
+    "url": "/17-react-portfolio/static/media/image.af826f61.svg"
+  },
+  {
+    "revision": "0f39f893474e22231e13bf6d8d9b2351",
+    "url": "/17-react-portfolio/static/media/logo.0f39f893.svg"
+  },
+  {
+    "revision": "b827b6fe25e5bcb7192aed25c5295efc",
+    "url": "/17-react-portfolio/static/media/logo.b827b6fe.svg"
+  },
+  {
+    "revision": "09d3d7ff8ec72d3cd422a35918598842",
+    "url": "/17-react-portfolio/static/media/mic.09d3d7ff.svg"
+  },
+  {
+    "revision": "443fdb89f439772c5540bb53d748d81f",
+    "url": "/17-react-portfolio/static/media/news-active.443fdb89.svg"
+  },
+  {
+    "revision": "981d628cefbee514822e944ce18ffc26",
+    "url": "/17-react-portfolio/static/media/news.981d628c.svg"
+  },
+  {
+    "revision": "cb730598580dc93d87b72aa96363bd43",
+    "url": "/17-react-portfolio/static/media/search-active.cb730598.svg"
+  },
+  {
+    "revision": "63a488c543aaeab550fcb6528448db87",
+    "url": "/17-react-portfolio/static/media/search.63a488c5.svg"
+  },
+  {
+    "revision": "1e83f06b4324d21c8364ca260e23c14d",
+    "url": "/17-react-portfolio/static/media/video-active.1e83f06b.svg"
+  },
+  {
+    "revision": "30fdcb528ac8aa5c27995f15605cc74c",
+    "url": "/17-react-portfolio/static/media/video.30fdcb52.svg"
+  }
+]);
